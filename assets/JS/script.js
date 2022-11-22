2500fa38dd2fd432488f4e64dafcb99a98225d96
Manuel Díaz <medc1991@gmail.com>
2022-11-21 22:34:47 -0400

    desafio-js6

---
+++ assets/JS/script.js
@@ -0,0 +1,23 @@
+pesoValue = document.querySelector("#pesoValue");
+tipoCambio = document.querySelector("#tipoCambio");
+fndBtn = document.querySelector("#fndBtn");
+totalTxt = document.querySelector("#totalTxt");
+
+const cambio = async () => {
+
+    try {
+        const response = await fetch(`https://mindicador.cl/api/${tipoCambio.value}`);
+        const data = await response.json()
+        const valorCambio = +data.serie[0].valor
+
+        totalTxt.textContent = pesoValue.value / valorCambio
+    } catch (e) {
+        totalTxt.textContent = e.message
+    }
+
+}
+
+fndBtn.addEventListener("click", () => {
+    cambio()
+})
+
